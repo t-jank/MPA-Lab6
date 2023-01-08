79a91104ca8b617140caae79d1a449e36da82942
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2023-01-08 14:02:39 +0100

    plot 1

--- urns.py
+++ urns.py
@@ -26,18 +26,25 @@ def balls_throw(n):
 
 nMin=10
 nMax=10000
-nStep=10
+nStep=13
 nRepeats=10
 
 for n in range(nMin,nMax,nStep):
     summ=0
     for r in range(0,nRepeats):
         summ+=balls_throw(n)
-    plt.scatter(n,summ/nRepeats,color='k')
-    plt.scatter(n,Harmonic_Number(n), color='hotpink')
+    if n==nMin:
+        plt.scatter(n,summ/nRepeats,color='k',label='simulation, 10 repeats for each n')
+        plt.scatter(n, 1.9*math.log(n)/math.log(math.log(n))-1.2, color='blue',label='1.9*log(n)/loglog(n)-1.2')
+    else:
+        plt.scatter(n,summ/nRepeats,color='k')
+        plt.scatter(n, 1.9*math.log(n)/math.log(math.log(n))-1.2, color='blue')
 #plt.xlim(left=0)
 #plt.xlim(right=nMax)
 plt.xlabel('n')
-plt.ylabel('No. balls in biggest urn')
-plt.title('No. balls in biggest urn')
+plt.ylabel('Number of balls in biggest urn')
+plt.title('Maximum load')
+plt.legend()
+plt.show()
+
 
